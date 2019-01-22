8f64f86dc13cf5cd2e79a1139503c240fbfb8849
skim037 <skim037@gold.ac.uk>
2019-01-22 10:37:00 +0000

    template added

--- lab-12-1/hello_world.py
+++ lab-12-1/hello_world.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import render_template
+import datetime
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World'
+def index():
+    return 'Index Page'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/time')
+def show_time():
+    t = str(datetime.datetime.now())
+    return 'time : %s ' % t
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=8000)
